919e2f4a986b2ebd69e1221569cc4804027d35e0
Jerin Philip <jphilip@ed.ac.uk>
2021-01-26 20:35:11 +0000

    Bugfix in batcher

--- src/translator/batcher.cpp
+++ src/translator/batcher.cpp
@@ -9,7 +9,7 @@ namespace bergamot {
 Batcher::Batcher(Ptr<Options> options) {
   max_input_tokens_ = options->get<int>("max-input-tokens");
   bucket_.resize(options->get<int>("max-input-sentence-tokens") + 1);
-  ABORT_IF(max_input_tokens_ >= bucket_.size(),
+  ABORT_IF(max_input_tokens_ < bucket_.size(),
            "max-input-sentence-tokens cannot be greater than max-input-tokens, "
            "batcher fail");
 }
